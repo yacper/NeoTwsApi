b4fb2a33bd55cd3afdd942e3f980a3911e546a84
yacper <yacper@gmail.com>
2024-03-12 13:17:24 +0800

    。

--- NeoTwsApi/Enums/ETifTws.cs
+++ NeoTwsApi/Enums/ETifTws.cs
@@ -14,7 +14,7 @@ public enum ETifTws
     GTD     = 2, // Good-Till-Date       ib的gtd，实际上是指定日期，必须提供时间，否则报错
     IOC     = 3, // Immediate-or-Cancel
     FOK     = 4, // Fill-or-Kill
-    DAY     = 5, // Day         // 当天，一般意义上的GTD
+    DAY     = 5, // Day         // 当天
     Minutes = 6, // Order are canceled if not filled in 5 minutes. https://interactivebrokers.github.io/tws-api/cryptocurrency.html
     OPG =7, // Use OPG to send a market-on-open (MOO) or limit-on-open (LOO) order.
 }
